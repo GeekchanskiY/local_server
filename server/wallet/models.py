41f515c2722928025720caf59888aac373b4a893
GeekanskiY <astrovskiy.04@gmail.com>
2023-02-18 13:48:21 +0300

    wallet models created

--- server/wallet/models.py
+++ server/wallet/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=60, default='United States Dollar')
+    short_name = models.CharField(max_length=10, default='USD')
+    USD_price = models.FloatField(default=1)
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    available_money = models.FloatField(default=0)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_DEFAULT, default="USD")
+
+
+class Outlay(models.Model):
+    amount = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    name = models.CharField(max_length=255)
+    comment = models.TextField(null=True, blank=True)
+
+
+class Income(models.Model):
+    amount = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    source = models.CharField(max_length=255)
+    comment = models.TextField(null=True, blank=True)
