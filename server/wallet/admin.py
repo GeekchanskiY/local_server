ef707ef0153dab68efbadf9b0a71a91d037e93ef
GeekanskiY <astrovskiy.04@gmail.com>
2023-02-18 14:17:00 +0300

    admin part ready

--- server/wallet/admin.py
+++ server/wallet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Currency, Income, Outlay, Wallet
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    fields = ('name', 'short_name', 'USD_price', )
+    list_display = ('name', 'USD_price', )
+    search_fields = ('name', )
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    fields = ('user', 'available_money', 'currency', )
+    list_filter = ('user', 'currency',)
+    list_display = ('id', 'user', )
+    search_fields = ('user', )
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    fields = ('amount', 'time', 'source', 'comment')
+    search_fields = ('source', )
